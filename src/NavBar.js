265a8b41ec38f7a8717c6070bb807f1e0ba1bf6f
hassanabdullah3924 <hassanabdullah3924@gmail.com>
2019-11-23 16:25:14 +0400

    Quick commit

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import LoginButton from './LoginButton'
-
+import AppContext from './AppContext'
 const NavBar = (prop) => {
+
+  const [globalState, setGlobalState] = useContext (AppContext);
+
     return (
       <nav className="navbar navbar-light bg-light">
         <a className="navbar-brand">
